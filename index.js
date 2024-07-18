8fff58b255f680d9f5ce4ba99b384db28f3e10ad
ivan-andriichak <picea.abies10@gmail.com>
2024-07-18 17:39:31 +0300

    hw-3.

--- index.js
+++ index.js
@@ -1,54 +1,34 @@
-const express = require('express');
+const express = require('express')
+const {read, write} = require('./fs.service')
 const app = express()
-
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-const users = [
-  {
-    id: 1,
-    name: 'Alice',
-    email: 'alice@example.com',
-    password: 'alicepassword123'
-  },
-  {
-    id: 2,
-    name: 'Bob',
-    email: 'bob@example.com',
-    password: 'bobpassword123'
-  },
-  {
-    id: 3,
-    name: 'Charlie',
-    email: 'charlie@example.com',
-    password: 'charliepassword123'
-  },
-  {
-    id: 4,
-    name: 'Diana',
-    email: 'diana@example.com',
-    password: 'dianapassword123'
-  },
-  {
-    id: 5,
-    name: 'Eve',
-    email: 'eve@example.com',
-    password: 'evepassword123'
-  }
-];
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
   try {
-    res.json(users)
+    const users = await read();
+    res.json(users);
   } catch (e) {
-    res.status(400).json(e.message);
+    res.status(500).json(e.message);
   }
-})
+});
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   try {
     const {name, email, password} = req.body;
 
+    if (!name || name.length < 3) {
+      return res.status(400).json('Name is required and should be at least 3 characters')
+    }
+    if (!email || !email.includes('@')) {
+      return res.status(400).json('Email is required and should be valid');
+    }
+    if (!password || password.length < 6) {
+      return res.status(400).json('Password is required and should be at least 6 characters')
+    }
+
+    const users = await read();
     const index = users.findIndex((user) => user.email === email)
     if (index !== -1) {
       return res.status(409).json('User with this email already exists')
@@ -60,57 +40,70 @@ app.post('/users', (req, res) => {
       password
     }
     users.push(newUser);
+    await write(users);
+
     res.status(201).json(newUser);
   } catch (e) {
-    res.status(400).json(e.message)
+    res.status(500).json(e.message);
   }
 })
 
-app.get('/users/:userId', (req, res) => {
+app.get('/users/:userId', async (req, res) => {
   try {
     const userId = Number(req.params.userId);
+
+    const users = await read();
     const user = users.find(user => user.id === userId);
     if (!user) {
       return res.status(404).json('User not found');
     }
     res.json(user);
   } catch (e) {
-    res.status(400).json(e.message);
+    res.status(500).json(e.message)
   }
-});
+})
 
-app.put('/users/:userId', (req, res) => {
+app.put('/users/:userId', async (req, res) => {
   try {
     const userId = Number(req.params.userId);
     const {name, email, password} = req.body;
+
+    const users = await read();
     const user = users.find(user => user.id === userId);
     if (!user) {
       return res.status(404).json('User not found')
     }
+
     if (name) user.name = name;
     if (email) user.email = email;
     if (password) user.password = password;
 
+    await write(users);
+
     res.status(201).json(user);
   } catch (e) {
-    res.status(400).json(e.message)
+    res.status(500).json(e.message)
   }
 });
 
-app.delete('/users/:userId', (req, res) => {
+app.delete('/users/:userId', async (req, res) => {
   try {
     const userId = Number(req.params.userId);
+
+    const users = await read();
     const index = users.findIndex(user => user.id === userId);
     if (index === -1) {
       return res.status(404).json('User not found')
     }
     users.splice(index, 1);
+    await write(users);
+
     res.sendStatus(204);
   } catch (e) {
-    res.status(400).json(e.message)
+    res.status(500).json(e.message)
   }
 })
 
 app.listen(3001, () => {
-  console.log("Server started on port 3001")
-});
+  console.log('Server is running on port 3001')
+})
