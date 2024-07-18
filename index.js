6a8634a0fef4567dca080310bd66f515b9511545
ivan-andriichak <picea.abies10@gmail.com>
2024-07-18 14:46:14 +0300

    lesson-3.

--- index.js
+++ index.js
@@ -1,40 +1,116 @@
-const path = require('node:path');
-const fs = require('node:fs/promises');
+const express = require('express');
+const app = express()
 
-const foo = async () => {
-  try {
-    const baseFolderPath = path.join(process.cwd(), 'baseFolder');
-    await fs.mkdir(baseFolderPath, {recursive: true});
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-    const folderNames = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5'];
-    const fileNames = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt'];
-
-    await Promise.allSettled(folderNames.map(async (folderName) => {
-      const folderPath = path.join(baseFolderPath, folderName);
-      await fs.mkdir(folderPath, {recursive: true});
-      await Promise.allSettled(fileNames.map(async (fileName) => {
-        await fs.writeFile(path.join(folderPath, fileName), "Hello!!!");
-      }));
-    }));
-
-    const folders = await fs.readdir(baseFolderPath);
-    for (const folder of folders) {
-      const folderPath = path.join(baseFolderPath, folder);
-      const stat = await fs.stat(folderPath);
-      console.log(`${folderPath} isDirectory: ${stat.isDirectory()}`);
-
-      const files = await fs.readdir(path.join(baseFolderPath, folder));
-      for (const file of files) {
-        const filePath = path.join(folderPath, file);
-        const stat = await fs.stat(filePath);
-        console.log(`${filePath} isDirectory: ${stat.isDirectory()}`);
-      }
-    }
-
-  } catch (e) {
-    console.error(e.message)
+const users = [
+  {
+    id: 1,
+    name: 'Alice',
+    email: 'alice@example.com',
+    password: 'alicepassword123'
+  },
+  {
+    id: 2,
+    name: 'Bob',
+    email: 'bob@example.com',
+    password: 'bobpassword123'
+  },
+  {
+    id: 3,
+    name: 'Charlie',
+    email: 'charlie@example.com',
+    password: 'charliepassword123'
+  },
+  {
+    id: 4,
+    name: 'Diana',
+    email: 'diana@example.com',
+    password: 'dianapassword123'
+  },
+  {
+    id: 5,
+    name: 'Eve',
+    email: 'eve@example.com',
+    password: 'evepassword123'
   }
+];
 
-}
+app.get('/users', (req, res) => {
+  try {
+    res.json(users)
+  } catch (e) {
+    res.status(400).json(e.message);
+  }
+})
 
-void foo()
+app.post('/users', (req, res) => {
+  try {
+    const {name, email, password} = req.body;
+
+    const index = users.findIndex((user) => user.email === email)
+    if (index !== -1) {
+      return res.status(409).json('User with this email already exists')
+    }
+    const newUser = {
+      id: users[users.length - 1].id + 1,
+      name,
+      email,
+      password
+    }
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } catch (e) {
+    res.status(400).json(e.message)
+  }
+})
+
+app.get('/users/:userId', (req, res) => {
+  try {
+    const userId = Number(req.params.userId);
+    const user = users.find(user => user.id === userId);
+    if (!user) {
+      return res.status(404).json('User not found');
+    }
+    res.json(user);
+  } catch (e) {
+    res.status(400).json(e.message);
+  }
+});
+
+app.put('/users/:userId', (req, res) => {
+  try {
+    const userId = Number(req.params.userId);
+    const {name, email, password} = req.body;
+    const user = users.find(user => user.id === userId);
+    if (!user) {
+      return res.status(404).json('User not found')
+    }
+    if (name) user.name = name;
+    if (email) user.email = email;
+    if (password) user.password = password;
+
+    res.status(201).json(user);
+  } catch (e) {
+    res.status(400).json(e.message)
+  }
+});
+
+app.delete('/users/:userId', (req, res) => {
+  try {
+    const userId = Number(req.params.userId);
+    const index = users.findIndex(user => user.id === userId);
+    if (index === -1) {
+      return res.status(404).json('User not found')
+    }
+    users.splice(index, 1);
+    res.sendStatus(204);
+  } catch (e) {
+    res.status(400).json(e.message)
+  }
+})
+
+app.listen(3001, () => {
+  console.log("Server started on port 3001")
+});
